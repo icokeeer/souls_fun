63deaca0d4c58e17e561d17fb8552a763a262a1c
Young <icoker@qq.com>
2019-11-22 01:08:52 +0800

    fix the output error.

--- souls/views.py
+++ souls/views.py
@@ -6,5 +6,5 @@ from .models import SoulsText
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
     soul_text = SoulsText.objects.order_by('?')[:1]
-    context = {'soul_text': soul_text}
-    return render(request, 'index.html', context)
+    # context = {'soul_text': soul_text}
+    return render(request, 'index.html', {'soul_text': soul_text})
